1a1693837b85e4b86df43e93b9d3112bbd0d05fb
Eersel,Sara S.C.M. van <sara.vaneersel@student.fontys.nl>
2023-05-08 11:11:57 +0000

    added install docker

--- python_template/docs/conf.py
+++ python_template/docs/conf.py
@@ -18,7 +18,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'myst_parser']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'myst_parser', 'sphinx_needs']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
